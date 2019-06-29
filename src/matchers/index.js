3b576f070775930518efe5ce264d998354eaebad
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-06-28 22:35:54 -0700

    feat(matchers): add toEqual

---
+++ src/matchers/index.js
@@ -0,0 +1,1 @@
+export { default as toEqual } from './toEqual';
---
+++ src/matchers/test/toEqual.spec.js
@@ -0,0 +1,10 @@
+import toEqual from '../toEqual';
+
+describe('toEqual', () => {
+  it('should match simple values', () => {
+    expect(toEqual(1, 1)).toBeTruthy();
+    expect(toEqual('onyx', 'onyx')).toBeTruthy();
+    expect(toEqual(0, 0)).toBeTruthy();
+    expect(toEqual(0, -1)).toBeFalsy();
+  });
+});
