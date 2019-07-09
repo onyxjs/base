5d01149023699bc88a520c2e12d88f16835aa32e
sh7dm <d3dx12.xx@gmail.com>
2019-07-09 12:30:36 +0300

    feat(spy): support custom implementations

--- packages/mock/src/spy.js
+++ packages/mock/src/spy.js
@@ -1,7 +1,7 @@
 import mock from './mock';
 
-const spy = (obj, prop) => {
-  obj[prop] = mock(obj[prop]);
+const spy = (obj, prop, impl = undefined) => {
+  obj[prop] = mock(impl || obj[prop]);
   return obj[prop];
 };
 
--- packages/mock/test/spy.spec.js
+++ packages/mock/test/spy.spec.js
@@ -13,4 +13,17 @@ describe('Spy', () => {
     expect(obj.fn(1, 2)).toBe(3);
     expect(spyFn.calls).toEqual([[1, 2], [1, 2]]);
   });
+
+  it('should create a spy function with custom implementation', () => {
+    const obj = {
+      fn: (a, b) => a + b,
+    };
+    const spyFn = spy(obj, 'fn', (a, b) => a * b);
+
+    expect(spyFn.calls).toEqual([]);
+    expect(spyFn(2, 2)).toBe(4);
+    expect(spyFn.calls).toEqual([[2, 2]]);
+    expect(obj.fn(2, 2)).toBe(4);
+    expect(spyFn.calls).toEqual([[2, 2], [2, 2]]);
+  });
 });
