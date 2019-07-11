5f8288db5ccc113e008a296d2eb8cb977597fc93
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-07-10 19:41:53 -0700

    feat(Core): Add describe and it.

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -4,4 +4,24 @@ module.exports = core;
 
 function core() {
     // TODO
+    // Describe fn
+    // xDescribe fn
+    // It fn
+    // xIt fn
+
+    const describe = (description, fn) => {
+      try {
+        fn();
+      } catch(err) {
+        return new Error('error', err);
+      }
+    };
+
+    const it = (description, fn) => {
+      try {
+        fn();
+      } catch(err) {
+        return new Error('error', err);
+      }
+    };
 }
