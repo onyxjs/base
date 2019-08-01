01f81ef4cf7f88b2e3f64a43b428829727d7ad51
sh7dm <d3dx12.xx@gmail.com>
2019-08-01 14:11:44 +0300

    perf(expect): fix test optimization

--- packages/matchers/test/expect.spec.ts
+++ packages/matchers/test/expect.spec.ts
@@ -1,13 +1,17 @@
-import _expect from '../src/index';
+import _expect from '../src/expect';
+
+const matchers = {
+  toStrictlyEqual: (a, b) => a === b,
+};
 
 describe('Expect', () => {
   it('should expect values', () => {
-    expect(() => _expect(1).toBe(1)).not.toThrow();
-    expect(() => _expect(1).toBe(2)).toThrow();
+    expect(() => _expect(matchers, 1).toStrictlyEqual(1)).not.toThrow();
+    expect(() => _expect(matchers, 1).toStrictlyEqual(2)).toThrow();
   });
 
   it('should not expect values', () => {
-    expect(() => _expect(1).not.toBe(1)).toThrow();
-    expect(() => _expect(1).not.toBe(2)).not.toThrow();
+    expect(() => _expect(matchers, 1).not.toStrictlyEqual(1)).toThrow();
+    expect(() => _expect(matchers, 1).not.toStrictlyEqual(2)).not.toThrow();
   });
 });
