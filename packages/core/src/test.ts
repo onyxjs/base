43c309344641d25b05e8317f00d2493f5abc743f
sh7dm <d3dx12.xx@gmail.com>
2019-08-06 14:17:25 +0300

    refactor(test): remove redundant code inherited from runnable

--- packages/core/src/test.ts
+++ packages/core/src/test.ts
@@ -1,14 +1,10 @@
 import Runnable from './runnable';
 
 export default class Test extends Runnable {
-  public description: string;
-  public fn: () => any;
   public type: string;
 
-  constructor(description: string, fn: () => any) {
-    super(description, fn);
-    this.description = description;
-    this.fn = fn;
+  constructor(description: string, fn: () => any, skip = false) {
+    super(description, fn, skip);
     this.type = 'Test';
   }
 }
