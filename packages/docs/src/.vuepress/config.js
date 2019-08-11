3b327852a2f031e6b98d5236ae8199793a02f2c8
sh7dm <d3dx12.xx@gmail.com>
2019-08-11 20:24:51 +0300

    docs: add Russian locale

--- packages/docs/src/.vuepress/config.js
+++ packages/docs/src/.vuepress/config.js
@@ -1,9 +1,50 @@
 module.exports = {
   themeConfig: {
-    sidebar: [
-      '/',
-      '/matchers/',
-      '/mock/',
-    ],
+    locales: {
+      '/': {
+        selectText: 'Languages',
+        label: 'English',
+        editLinkText: 'Edit this page on GitHub',
+        serviceWorker: {
+          updatePopup: {
+            message: "New content is available.",
+            buttonText: "Refresh",
+          },
+        },
+        sidebar: [
+          '/',
+          '/matchers/',
+          '/mock/',
+        ],
+      },
+      '/ru/': {
+        selectText: 'Языки',
+        label: 'Русский',
+        editLinkText: 'Редактировать эту страницу на GitHub',
+        serviceWorker: {
+          updatePopup: {
+            message: "Доступна новая информация.",
+            buttonText: "Обновить",
+          },
+        },
+        sidebar: [
+          '/ru/',
+          '/ru/matchers/',
+          '/ru/mock/',
+        ],
+      },
+    },
+  },
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'Onyx',
+      description: 'Onyx is a JavaScript testing framework',
+    },
+    '/ru/': {
+      lang: 'ru-RU',
+      title: 'Onyx',
+      description: 'Onyx это фреймворк для тестированя JavaScript кода.',
+    },
   },
 }
