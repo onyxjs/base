06710e6be9239cbf5e0d7b04220fa0c25da4360a
sh7dm <d3dx12.xx@gmail.com>
2019-08-14 13:08:06 +0300

    refactor(runner): refactor code

--- packages/core/src/runner.ts
+++ packages/core/src/runner.ts
@@ -2,41 +2,26 @@ import { EventEmitter } from 'events';
 import Suite from '../src/suite';
 import Test from '../src/test';
 
-/**
- * @class
- * @param {Suite} suite
- */
 export default class Runner extends EventEmitter {
-  public suite: Suite;
-
-  constructor(suite: Suite) {
-    super();
-    this.suite = suite;
-  }
-
-  /**
-  * @public
-  * @param {Suite} suite
-  * @param {Runnable} fn
-  */
-  public runChildren(suite: Suite, fn: () => any) {}
-
   /**
    * @public
    * @param {Suite} suite 
-   * @param {Runnable} fn 
+   * @param {Function} fn 
    */
-  public runSuite(suite: Suite, fn: () => any) {
+  public static runSuite(suite: Suite, fn: () => any) {
     for (let child of suite.children) {
       if (child.type === 'Suite') {
-        this.runSuite((suite), () => {
-          fn();
-        });
+        this.runSuite(child as Suite, fn);
+      } else {
+        this.runTest(child as Test, fn);
       }
     }
-
-    this.runChildren(suite, () => {
-      fn();
-    });
   }
+
+  /**
+   * @public
+   * @param {Test} test 
+   * @param {Function} fn 
+   */
+  public static runTest(test: Test, fn: () => any) {}
 }
