a7358ddac6b8b089baa830941734e5879b507a8a
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-08-14 01:18:40 -0700

    refactor(Suite): Update suite.
    Add method to get full description and method to add children tests. Abstract status and type
    properties to runnable.

--- packages/core/src/suite.ts
+++ packages/core/src/suite.ts
@@ -1,10 +1,26 @@
 import Runnable from './runnable';
 
 export default class Suite extends Runnable {
-  public type: string;
+  public children: Runnable[];
+  public root: Suite | undefined;
+  public total: number;
 
-  constructor(description: string, fn: () => any, skip = false) {
-    super(description, fn);
-    this.type = 'Suite';
+  constructor(description: string, fn: () => any, skip = false, type = 'Suite') {
+    super(description, fn, type);
+    this.children = [];
+    this.total = 0;
+    this.type = type;
+  }
+
+  public addChild(child: Runnable) {
+    this.root = this;
+    this.children.push(child);
+  }
+
+  public getFullDesc(): string {
+    if (this.root) {
+      return this.root.getFullDesc() + ' ' + this.description;
+    }
+    return this.description;
   }
 }
