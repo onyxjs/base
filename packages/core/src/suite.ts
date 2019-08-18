468c38a7986879f4188162f512ef737da8fb9865
sh7dm <d3dx12.xx@gmail.com>
2019-08-18 09:54:27 +0300

    refactor(core): major refactor

--- packages/core/src/suite.ts
+++ packages/core/src/suite.ts
@@ -1,10 +1,11 @@
+import Result, { Status } from './result';
 import Runnable from './runnable';
 
 export default class Suite extends Runnable {
   public children: Runnable[];
 
-  constructor(description: string, fn: () => any, skip = false) {
-    super(description, fn, skip);
+  constructor(description: string, skip = false) {
+    super(description, skip);
     this.children = [];
   }
 
@@ -18,5 +19,23 @@ export default class Suite extends Runnable {
       return this.parent.getFullDesc() + ' ' + this.description;
     }
     return this.description;
+  }
+
+  /**
+   * Run a `Suite` instance return `Runnable` status:
+   * @public
+   * @return {Result}
+   */
+  public run(): Result {
+    let newStatus = Status.Passed;
+    for (const child of this.children) {
+      const result = child.run();
+      if (result.status === Status.Errored && newStatus === Status.Passed) { newStatus = Status.Errored; }
+      if (result.status === Status.Failed && newStatus === Status.Passed) { newStatus = Status.Failed; }
+      this.result.addMessages(...result.messages);
+    }
+
+    this.result.status = newStatus;
+    return this.result;
   }
 }
