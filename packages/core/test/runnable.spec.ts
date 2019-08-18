714170eda75af446c658a6906b64da43d9da1efb
sh7dm <d3dx12.xx@gmail.com>
2019-08-18 10:45:40 +0300

    test(runnable): update tests

--- packages/core/test/runnable.spec.ts
+++ packages/core/test/runnable.spec.ts
@@ -1,62 +1,12 @@
 import Runnable from '../src/runnable';
+import Suite from '../src/suite';
 
 describe('Runnable', () => {
-  it('should run', () => {
-    const fn = jest.fn();
-    const runnable = new Runnable('test', fn);
+  it('should get full description', () => {
+    const parent = new Suite('parent');
+    const child = new Runnable('child');
+    child.parent = parent;
 
-    expect(fn).toHaveBeenCalledTimes(0);
-    expect(runnable.run()).toMatchSnapshot();
-    expect(fn).toHaveBeenCalledTimes(1);
-  });
-
-  it('should return isDone', () => {
-    const runnable = new Runnable('test', () => null);
-
-    expect(runnable.isDone()).toBeFalsy();
-    runnable.run();
-    expect(runnable.isDone()).toBeTruthy();
-  });
-
-  it('should skip', () => {
-    const fn = jest.fn();
-    const runnable = new Runnable('test', fn, true);
-
-    expect(runnable.run()).toMatchSnapshot();
-    expect(fn).toHaveBeenCalledTimes(0);
-  });
-
-  it('should error', () => {
-    const fn = () => {
-      throw new Error('Fatal error');
-    };
-    const runnable = new Runnable('test', fn);
-
-    expect(runnable.run()).toMatchSnapshot();
-  });
-
-  class ExpectError extends Error {
-    constructor(message: string) {
-      super(message);
-      this.name = 'ExpectError';
-    }
-  }
-
-  it('should fail', () => {
-    const fn = () => {
-      throw new ExpectError('Expect failed');
-    };
-    const runnable = new Runnable('test', fn);
-
-    expect(runnable.run()).toMatchSnapshot();
-  });
-
-  it('should run asynchronously', async () => {
-    const fn = jest.fn();
-    const runnable = new Runnable('test', fn);
-
-    expect(fn).toHaveBeenCalledTimes(0);
-    expect(await runnable.asyncRun()).toMatchSnapshot();
-    expect(fn).toHaveBeenCalledTimes(1);
-  });
+    expect(child.getFullDescription()).toBe('parent child');
+  })
 });
