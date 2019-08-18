492873189e9f8fbd80b3609fc14a374c20110940
sh7dm <d3dx12.xx@gmail.com>
2019-08-18 18:32:08 +0300

    feat(core): remove runner, export all the stuff

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,9 @@
-module.exports = core;
+import describe, { root } from './interface';
+(global as any).describe = describe;
+(global as any).root = root;
 
-function core() {
-  // Onyx core fn:
-}
+export { root, ItFn, It, DescribeCallback, default as describe } from './interface';
+export { Status, default as Result } from './result';
+export { default as Runnable } from './runnable';
+export { default as Test } from './test';
+export { default as Suite } from './suite';
