d7ded750fd47f50f03b3b9652b98b2235016ee45
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2019-12-30 10:57:19 +0300

    test(runnable): add tests for events

--- packages/core/test/runnable.spec.ts
+++ packages/core/test/runnable.spec.ts
@@ -1,7 +1,7 @@
 import { Status } from '../src/result';
 import Runnable from '../src/runnable';
 import Suite, { rootSymbol } from '../src/suite';
-import Test from '../src/test';
+import Test, { isTest } from '../src/test';
 
 describe('Runnable', () => {
   it('should get full description', () => {
@@ -25,5 +25,65 @@ describe('Runnable', () => {
     const runnable = new Runnable('runnable');
 
     expect((await runnable.asyncRun()).status).toBe(Status.Skipped);
+  });
+
+  describe('events', () => {
+    it('start', () => {
+      const runnable = new Runnable('runnable');
+
+      const fn = jest.fn();
+      runnable.on('start', fn);
+
+      runnable.doStart();
+      expect(fn).toHaveBeenCalledTimes(1);
+    });
+
+    it('pass', () => {
+      const runnable = new Runnable('runnable');
+      const fn = jest.fn();
+      runnable.on('pass', fn);
+
+      const end = jest.fn();
+      runnable.on('end', end);
+
+      runnable.doPass();
+      expect(fn).toHaveBeenCalledTimes(1);
+      expect(end).toHaveBeenCalledTimes(1);
+    });
+
+    it('fail', () => {
+      const runnable = new Runnable('runnable');
+      const fn = jest.fn();
+      runnable.on('fail', fn);
+
+      const end = jest.fn();
+      runnable.on('end', end);
+
+      runnable.doFail();
+      expect(fn).toHaveBeenCalledTimes(1);
+      expect(end).toHaveBeenCalledTimes(1);
+    });
+
+    it('skip', () => {
+      const runnable = new Runnable('runnable');
+      const fn = jest.fn();
+      runnable.on('skip', fn);
+
+      const end = jest.fn();
+      runnable.on('end', end);
+
+      runnable.doSkip();
+      expect(fn).toHaveBeenCalledTimes(1);
+      expect(end).toHaveBeenCalledTimes(1);
+    });
+
+    it('end', () => {
+      const runnable = new Runnable('runnable');
+      const end = jest.fn();
+      runnable.on('end', end);
+
+      runnable.doEnd();
+      expect(end).toHaveBeenCalledTimes(1);
+    });
   });
 });
