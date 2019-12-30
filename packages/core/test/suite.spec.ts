08bffe855f65f365a63d4fc0bbbe2814cdacd538
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2019-12-30 09:58:17 +0300

    chore: remove obsolete tests

--- packages/core/test/suite.spec.ts
+++ packages/core/test/suite.spec.ts
@@ -51,23 +51,6 @@ describe('Suite', () => {
   });
 
   // tslint:disable-next-line:max-classes-per-file
-  class ErroringRunnable extends Runnable {
-    public run() {
-      this.result.addMessages('FATAL ERROR!');
-      this.result.status = Status.Errored;
-      return this.result;
-    }
-  }
-  it('should error and collect messages', () => {
-    const child = new ErroringRunnable('desc');
-    const suite = new Suite('Suite');
-    suite.addChild(child);
-    suite.run();
-
-    expect(suite.result).toMatchSnapshot();
-  });
-
-  // tslint:disable-next-line:max-classes-per-file
   class FailingRunnable extends Runnable {
     public run() {
       this.result.addMessages('FAIL!');
