fd938cc1632415bf9e2bf88a4281124c33929ac7
Kevin Wang <kcwang06@gmail.com>
2020-02-24 14:47:00 -0500

    edited config file

--- packages/docs/.vuepress/config.js
+++ packages/docs/.vuepress/config.js
@@ -1,80 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-  base: '/onyx/',
-  plugins: {
-    '@vuepress/pwa': {
-      serviceWorker: true,
-      updatePopup: {
-        '/': {
-          message: "New content is available.",
-          buttonText: "Refresh",
-        },
-        '/ru/': {
-          message: "Доступна новая информация.",
-          buttonText: "Обновить",
-        },
-      },
-    },
-    '@vuepress/nprogress': {},
-    '@vuepress/back-to-top': {},
-  },
-  themeConfig: {
-    locales: {
-      '/': {
-        selectText: 'Languages',
-        label: 'English',
-        editLinkText: 'Edit this page on GitHub',
-        sidebar: [
-          '/',
-          {
-            title: '@onyx/matchers',
-            path: '/matchers/',
-            children: [
-              '/matchers/matchers/to-be',
-              '/matchers/matchers/to-be-defined',
-              '/matchers/matchers/to-be-false',
-              '/matchers/matchers/to-be-instance-of',
-              '/matchers/matchers/to-be-nan',
-              '/matchers/matchers/to-be-null',
-              '/matchers/matchers/to-be-true',
-              '/matchers/matchers/to-be-type-of',
-              '/matchers/matchers/to-be-undefined',
-              '/matchers/matchers/to-contain',
-              '/matchers/matchers/to-equal',
-              '/matchers/matchers/to-have-length',
-              '/matchers/matchers/to-strictly-equal',
-              '/matchers/matchers/to-throw.md'
-            ]
-          },
-          '/mock/',
-        ],
-      },
-      '/ru/': {
-        selectText: 'Языки',
-        label: 'Русский',
-        editLinkText: 'Редактировать эту страницу на GitHub',
-        sidebar: [
-          '/ru/',
-          '/ru/matchers/',
-          '/ru/mock/',
-        ],
-      },
-    },
-  },
-  locales: {
-    '/': {
-      lang: 'en-US',
-      title: 'Onyx',
-      description: 'Onyx is a JavaScript testing framework',
-    },
-    '/ru/': {
-      lang: 'ru-RU',
-      title: 'Onyx',
-      description: 'Onyx это фреймворк для тестирования JavaScript кода.',
-    },
-  },
-}
-=======
   base: "/onyx/",
   plugins: {
     "@vuepress/pwa": {
@@ -188,4 +112,3 @@ module.exports = {
     }
   }
 };
->>>>>>> 502df612625d1b89d03e2b2c1507de9161b6c3b3
