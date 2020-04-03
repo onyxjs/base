0533e5f4832e15b0984c2ec91c215d5305da5a82
Elijah Kotyluk <elijah@elijahkotyluk.com>
2020-04-03 11:43:25 -0700

    docs(test): add description comments to methods and helper functions

--- packages/core/src/test.ts
+++ packages/core/src/test.ts
@@ -3,28 +3,29 @@ import Runnable, { isRunnable, RunnableOptions, RunnableTypes } from './runnable
 import { RunOptions } from './runner';
 import Suite from './suite';
 
-export type testFn = () => void | Promise<any>;
+export type TestFn = () => (void | Promise<any>);
 
+/**
+ * @description Checks if the passed value is a `Runnable` instance of type `Test`.
+ */
 export const isTest = (v: unknown): v is Test => {
   if (!isRunnable(v)) { return false; }
   return v.type === RunnableTypes.Test;
 };
 
 export default class Test extends Runnable {
-  public fn: testFn;
+  public fn: TestFn;
   public type = RunnableTypes.Test;
 
   /* istanbul ignore next */
-  constructor(description: string, fn: testFn, options: Partial<RunnableOptions> = {}, parent: Suite | null) {
+  constructor(description: string, fn: TestFn, options: Partial<RunnableOptions> = {}, parent: Suite | null) {
     super(description, options, parent);
     this.fn = fn;
     this.parent = parent;
   }
 
   /**
-   * Run a `Test` instance asynchronously return `Runnable` status:
-   * @public
-   * @return {Promise<Result>}
+   * @description Run a `Test` instance.
    */
   public async run(options?: Partial<RunOptions>): Promise<Result> {
     if (this.options.skip || this.options.todo) {
