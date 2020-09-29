2572412d10ec4fa17a7a92292709612bbad0cf87
Elijah Kotyluk <elijah@elijahkotyluk.com>
2020-09-28 22:40:55 -0700

    feat(core-types): add interface and hooks types

--- packages/types/src/Core.ts
+++ packages/types/src/Core.ts
@@ -85,7 +85,7 @@ export interface Test extends Runnable {
   run(options: Partial<RunOptions>): Promise<Result>;
 }
 
-// Suite
+// Suite stats
 export interface SuiteStats {
   total: number;
   pending: number;
@@ -98,12 +98,54 @@ export interface SuiteStats {
   time: number;
 }
 
+// Hooks
+export type HookFn = (() => void) | (() => Promise<void>);
+export type Hook = HookFn[];
+export interface Hooks {
+  beforeAll: Hook;
+  afterAll: Hook;
+  beforeEach: Hook;
+  afterEach: Hook;
+}
+export type HookName = keyof Hooks;
+
 // Root Symbol
 export const rootSymbol = Symbol('isRoot');
 
+// Suite
 export interface Suite extends Runnable {
   children: Runnable[];
+  type: RunnableTypes.Suite;
+  options: RunnableOptions;
+  hooks: Hooks;
+  failed: number;
 
+  new(description: string, options: Partial<RunnableOptions>, parent: Suite): Suite;
+
+  invokeHook(name: HookName): void;
+
+  addChildren(...children: Runnable[]): void;
+
+  isRoot(): boolean;
+
+  run(options?: Partial<RunOptions>): Promise<Result>;
+
+  getStats(): SuiteStats;
+}
+
+// Interface
+export type DescribeFn = (description: string, fn: () => void) => Suite;
+
+export interface Describe extends DescribeFn {
+  skip: DescribeFn;
+  todo: DescribeFn;
+}
+
+export type ItFn = (description: string, fn: () => void) => Test;
+
+export interface It extends ItFn {
+  skip: ItFn;
+  todo: ItFn;
 }
 
 // Bail Error
@@ -111,13 +153,14 @@ export interface BailError extends Error {
   new(message: string): BailError;
 }
 
-
+// Run Options
 export type RunOptions = {
   bail: boolean,
   sequential: boolean;
   timeout: number;
 }
 
+// Runner
 export interface Runner {
   rootSuite: Suite;
   options: RunOptions;
