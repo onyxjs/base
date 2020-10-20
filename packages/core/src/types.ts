f0f98d87e44c4019cb885315cc0a3ddec0452ceb
Elijah Kotyluk <elijah@elijahkotyluk.com>
2020-10-20 00:17:55 -0700

    refactor(types): expand onyx events

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -2,9 +2,23 @@
  * @description Events used for tests and suites
  */
 export enum OnyxEvents {
-  End = 'end',
-  Fail = 'fail',
-  Pass = 'pass',
-  Skip = 'skip',
-  Start = 'start',
+  AfterAll = 'afterAll',
+  BeforeAll = 'beforeAll',
+  afterEach = 'afterEach',
+  BeforeEach = 'beforeEach',
+  SuiteEnd = 'end',
+  SuiteFail = 'fail',
+  SuitePass = 'pass',
+  SuiteSkip = 'skip',
+  SuiteStart = 'start',
+  TestEnd = 'end',
+  TestFail = 'fail',
+  TestPass = 'pass',
+  TestSkip = 'skip',
+  TestStart = 'start',
 }
+
+export type OnyxEvent = OnyxEvents.SuiteEnd | OnyxEvents.SuiteFail | OnyxEvents.SuitePass
+            | OnyxEvents.SuiteSkip | OnyxEvents.SuiteStart | OnyxEvents.TestEnd | OnyxEvents.TestFail
+            | OnyxEvents.TestPass | OnyxEvents.TestSkip | OnyxEvents.TestStart | OnyxEvents.AfterAll
+            | OnyxEvents.BeforeAll | OnyxEvents.BeforeEach | OnyxEvents.afterEach;
