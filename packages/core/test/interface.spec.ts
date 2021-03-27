2dc89cd7d3aadb3f5d67e827016c59565fdf6f60
Elijah Kotyluk <elijah@elijahkotyluk.com>
2021-03-27 13:38:06 -0700

    test(core): fix tests.

--- packages/core/test/interface.spec.ts
+++ packages/core/test/interface.spec.ts
@@ -9,168 +9,168 @@ import {
   it as $it,
   root,
   setCurrentRoot,
-} from '../src/interface'
-import Suite from '../src/suite'
+} from '../src/interface';
+import Suite from '../src/suite';
 
-const noop = () => null
+const noop = () => null;
 
 describe('Interface', () => {
   beforeEach(() => {
-    root.children.length = 0 // clean up root to simplify snapshots
-  })
+    root.children.length = 0; // clean up root to simplify snapshots
+  });
 
   it('should create suites', () => {
-    const warn = console.warn
-    console.warn = noop
+    const warn = console.warn;
+    console.warn = noop;
 
-    const suite = $describe('test 1', noop)
+    const suite = $describe('test 1', noop);
 
-    expect(suite).toMatchSnapshot()
+    expect(suite).toMatchSnapshot();
 
-    console.warn = warn
-  })
+    console.warn = warn;
+  });
 
   it('should add suites to root', () => {
-    const warn = console.warn
-    console.warn = noop
+    const warn = console.warn;
+    console.warn = noop;
 
-    $describe('test 2', noop)
+    $describe('test 2', noop);
 
-    expect(root).toMatchSnapshot()
+    expect(root).toMatchSnapshot();
 
-    console.warn = warn
-  })
+    console.warn = warn;
+  });
 
   it('should create skipped suites', () => {
-    const suite = $describe.skip('test 3', noop)
+    const suite = $describe.skip('test 3', noop);
 
-    expect(suite.options.skip).toBeTruthy()
-  })
+    expect(suite.options.skip).toBeTruthy();
+  });
 
   it('should create todo suites', () => {
-    const suite = $describe.todo('test 3', noop)
+    const suite = $describe.todo('test 3', noop);
 
-    expect(suite.options.todo).toBeTruthy()
-  })
+    expect(suite.options.todo).toBeTruthy();
+  });
 
-  it.skip('should create tests inside of suites', () => {
+  it('should create tests inside of suites', () => {
     const suite = $describe.skip('test 4', () => {
-      $it('child 1', noop)
-    })
+      $it('child 1', noop);
+    });
 
-    expect(suite.children[0]).toMatchSnapshot()
-    //expect(suite.children[0].parent.parent).toBe(root);
-  })
+    expect(suite.children[0]).toMatchSnapshot();
+    expect(suite.children[0].parent!.parent).toBe(root);
+  });
 
   it('should create skipped tests inside of suites', () => {
     const suite = $describe.skip('test 5', () => {
-      $it.skip('child 1', noop)
-    })
+      $it.skip('child 1', noop);
+    });
 
-    expect(suite.children[0]).toMatchSnapshot()
-  })
+    expect(suite.children[0]).toMatchSnapshot();
+  });
 
   it('should create todo tests inside of suites', () => {
     const suite = $describe.skip('test 5', () => {
-      $it.todo('child 1', noop)
-    })
+      $it.todo('child 1', noop);
+    });
 
-    expect(suite.children[0]).toMatchSnapshot()
-  })
+    expect(suite.children[0]).toMatchSnapshot();
+  });
 
   it('should update currentRoot', () => {
-    expect(currentRoot).toBe(root)
+    expect(currentRoot).toBe(root);
 
     $describe('root2', () => {
-      $it('child 1', noop) // avoid warnings
+      $it('child 1', noop); // avoid warnings
 
-      expect(currentRoot).not.toBe(root)
-      expect(currentRoot.parent).toBe(root)
-      expect(currentRoot.getFullDescription()).toBe('root2')
-    })
+      expect(currentRoot).not.toBe(root);
+      expect(currentRoot.parent).toBe(root);
+      expect(currentRoot.getFullDescription()).toBe('root2');
+    });
 
-    expect(currentRoot).toBe(root)
-  })
+    expect(currentRoot).toBe(root);
+  });
 
   it('should handle nested suites', () => {
-    expect(currentRoot).toBe(root)
+    expect(currentRoot).toBe(root);
 
     $describe('module', () => {
-      expect(currentRoot.getFullDescription()).toBe('module')
-      $it('test 1', noop)
-      $it('test 2', noop)
+      expect(currentRoot.getFullDescription()).toBe('module');
+      $it('test 1', noop);
+      $it('test 2', noop);
 
       $describe('component 1', () => {
-        expect(currentRoot.getFullDescription()).toBe('module -> component 1')
-        $it('test 1', noop)
-        $it('test 2', noop)
-      })
+        expect(currentRoot.getFullDescription()).toBe('module -> component 1');
+        $it('test 1', noop);
+        $it('test 2', noop);
+      });
 
       $describe('component 2', () => {
-        expect(currentRoot.getFullDescription()).toBe('module -> component 2')
-        $it('test 1', noop)
-        $it('test 2', noop)
+        expect(currentRoot.getFullDescription()).toBe('module -> component 2');
+        $it('test 1', noop);
+        $it('test 2', noop);
 
         $describe('method 1', () => {
-          expect(currentRoot.getFullDescription()).toBe('module -> component 2 -> method 1')
-          $it('test 1', noop)
-          $it('test 2', noop)
-        })
+          expect(currentRoot.getFullDescription()).toBe('module -> component 2 -> method 1');
+          $it('test 1', noop);
+          $it('test 2', noop);
+        });
 
         $describe('method 2', () => {
-          expect(currentRoot.getFullDescription()).toBe('module -> component 2 -> method 2')
-          $it('test 1', noop)
-          $it('test 2', noop)
-        })
-      })
-    })
+          expect(currentRoot.getFullDescription()).toBe('module -> component 2 -> method 2');
+          $it('test 1', noop);
+          $it('test 2', noop);
+        });
+      });
+    });
 
-    expect(root).toMatchSnapshot()
-  })
+    expect(root).toMatchSnapshot();
+  });
 
   it('should throw if it is called without describe context', () => {
-    expect(() => $it('test 1', noop)).toThrow('"test 1" "it" should not be called outside of "describe" block')
-  })
+    expect(() => $it('test 1', noop)).toThrow('"test 1" "it" should not be called outside of "describe" block');
+  });
 
   it('should warn if no children was created', () => {
-    const warn = console.warn
-    console.warn = jest.fn()
+    const warn = console.warn;
+    console.warn = jest.fn();
 
-    $describe('empty', noop)
-    expect(console.warn).toHaveBeenCalledWith(`suite "empty" doesn't have any child tests or suites.`)
+    $describe('empty', noop);
+    expect(console.warn).toHaveBeenCalledWith(`suite "empty" doesn't have any child tests or suites.`);
 
-    console.warn = warn
-  })
+    console.warn = warn;
+  });
 
   it('should get and set current root', () => {
-    expect(getCurrentRoot()).toBe(root)
+    expect(getCurrentRoot()).toBe(root);
 
-    setCurrentRoot({} as Suite)
-    expect(getCurrentRoot()).toBe(root)
+    setCurrentRoot({} as Suite);
+    expect(getCurrentRoot()).toBe(root);
 
-    setCurrentRoot(new Suite('new root', { skip: false, todo: false }, null))
-    expect(getCurrentRoot().description).toBe('new root')
+    setCurrentRoot(new Suite('new root', { skip: false, todo: false }, null));
+    expect(getCurrentRoot().description).toBe('new root');
 
-    setCurrentRoot(root)
-    expect(getCurrentRoot()).toBe(root)
-  })
+    setCurrentRoot(root);
+    expect(getCurrentRoot()).toBe(root);
+  });
 
   it('should add hooks to current suite', () => {
     $describe('root', () => {
-      $beforeAll(() => null)
-      $afterEach(() => null)
+      $beforeAll(() => null);
+      $afterEach(() => null);
       $describe('child', () => {
-        $it('test', () => null)
+        $it('test', () => null);
 
-        $beforeAll(() => null)
-        $beforeAll(() => undefined)
-        $beforeEach(() => null)
-        $afterEach(async () => null)
-        $afterAll(() => null)
+        $beforeAll(() => null);
+        $beforeAll(() => undefined);
+        $beforeEach(() => null);
+        $afterEach(async () => null);
+        $afterAll(() => null);
 
-        expect(currentRoot.hooks).toMatchSnapshot()
-      })
-      expect(currentRoot.hooks).toMatchSnapshot()
-    })
-  })
-})
+        expect(currentRoot.hooks).toMatchSnapshot();
+      });
+      expect(currentRoot.hooks).toMatchSnapshot();
+    });
+  });
+});
--- packages/core/test/suite.spec.ts
+++ packages/core/test/suite.spec.ts
@@ -1,223 +1,222 @@
-import { Status } from '../src/result'
-import Suite, { rootSymbol } from '../src/suite'
-import Test, { TestFn } from '../src/test'
+import { Status } from '../src/result';
+import Runnable from '../src/runnable';
+import Suite, { isSuite, rootSymbol } from '../src/suite';
+import Test from '../src/test';
 
-describe.skip('Suite', () => {
+describe('Suite', () => {
   const defaultOpts = {
     skip: false,
     todo: false,
-  }
-  
-  const testFn: TestFn = () => {
-    return
-  }
+  };
 
   it('should check if is root', () => {
-      const suite = new Suite('suite', defaultOpts, null)
+      const suite = new Suite('suite', defaultOpts, null);
 
-      expect(suite.isRoot()).toBeFalsy()
+      expect(suite.isRoot()).toBeFalsy();
 
-      suite[rootSymbol] = true
-      expect(suite.isRoot()).toBeTruthy()
-  })
+      suite[rootSymbol] = true;
+      expect(suite.isRoot()).toBeTruthy();
+  });
 
-  /*class FailingRunnable extends Runnable {
+  // tslint:disable-next-line:max-classes-per-file
+  class FailingRunnable extends Runnable {
     public async run() {
       this.result.addMessages('FAIL!');
       this.result.status = Status.Failed;
       return this.result;
     }
-  }*/
+  }
 
-  class PassingTest extends Test {
+  // tslint:disable-next-line:max-classes-per-file
+  class PassingRunnable extends Runnable {
     public async run() {
-      this.result.addMessages('OK')
-      this.result.status = Status.Passed
-      return this.result
+      this.result.addMessages('OK');
+      this.result.status = Status.Passed;
+      return this.result;
     }
   }
 
   it('should pass', async () => {
-    const child = new Test('child', jest.fn(), defaultOpts, null)
-    const parent = new Suite('parent', defaultOpts, null)
-    parent.addChildren(child)
+    const child = new Test('child', jest.fn(), defaultOpts, null);
+    const parent = new Suite('parent', defaultOpts, null);
+    parent.addChildren(child);
 
-    const start = jest.fn()
-    parent.on('start', start)
-    const pass = jest.fn()
-    parent.on('pass', pass)
-    const end = jest.fn()
-    parent.on('end', end)
+    const start = jest.fn();
+    parent.on('start', start);
+    const pass = jest.fn();
+    parent.on('pass', pass);
+    const end = jest.fn();
+    parent.on('end', end);
 
-    const promise = parent.run()
+    const promise = parent.run();
 
-    expect(start).toHaveBeenCalledTimes(1)
+    expect(start).toHaveBeenCalledTimes(1);
 
-    expect((await promise).status).toBe(Status.Passed)
-    expect(pass).toHaveBeenCalledTimes(1)
-    expect(end).toHaveBeenCalledTimes(1)
-  })
+    expect((await promise).status).toBe(Status.Passed);
+    expect(pass).toHaveBeenCalledTimes(1);
+    expect(end).toHaveBeenCalledTimes(1);
+  });
 
   it('should run sequentially', async () => {
-    const suite = new Suite('Suite', defaultOpts, null)
-    const child = new PassingTest('desc', testFn, defaultOpts, suite)
-    const child1 = new PassingTest('desc', testFn, defaultOpts, suite)
-    const child2 = new PassingTest('desc', testFn, defaultOpts, suite)
-    const child3 = new PassingTest('desc', testFn, defaultOpts, suite)
-    const child4 = new PassingTest('desc', testFn, defaultOpts, suite)
-    suite.addChildren(child, child1, child2, child3, child4)
+    const suite = new Suite('Suite', defaultOpts, null);
+    const child = new PassingRunnable('desc', defaultOpts, suite);
+    const child1 = new PassingRunnable('desc', defaultOpts, suite);
+    const child2 = new PassingRunnable('desc', defaultOpts, suite);
+    const child3 = new PassingRunnable('desc', defaultOpts, suite);
+    const child4 = new PassingRunnable('desc', defaultOpts, suite);
+    suite.addChildren(child, child1, child2, child3, child4);
 
-    await suite.run({ sequential: true })
-    expect(suite.getStats().done).toBe(5)
-  })
+    await suite.run({ sequential: true });
+    expect(suite.getStats().done).toBe(5);
+  });
 
   it('should fail', async () => {
-    const fn = jest.fn()
+    const fn = jest.fn();
 
-    const err = new Error('FAIL!')
-    const child = new Test('child 1', () => { throw err }, defaultOpts, null)
-    const passingChild = new Test('child 2', fn, defaultOpts, null)
-    const parent = new Suite('parent', defaultOpts, null)
-    parent.addChildren(child)
-    parent.addChildren(passingChild)
+    const err = new Error('FAIL!');
+    const child = new Test('child 1', () => { throw err; }, defaultOpts, null);
+    const passingChild = new Test('child 2', fn, defaultOpts, null);
+    const parent = new Suite('parent', defaultOpts, null);
+    parent.addChildren(child);
+    parent.addChildren(passingChild);
 
-    const start = jest.fn()
-    parent.on('start', start)
-    const fail = jest.fn()
-    parent.on('fail', fail)
-    const end = jest.fn()
-    parent.on('end', end)
+    const start = jest.fn();
+    parent.on('start', start);
+    const fail = jest.fn();
+    parent.on('fail', fail);
+    const end = jest.fn();
+    parent.on('end', end);
 
-    expect((await parent.run()).status).toBe(Status.Failed)
+    expect((await parent.run()).status).toBe(Status.Failed);
 
-    expect(fn).toHaveBeenCalledTimes(1)
-    expect(start).toHaveBeenCalledTimes(1)
-    expect(fail).toHaveBeenCalledTimes(1)
-    expect(end).toHaveBeenCalledTimes(1)
-  })
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(start).toHaveBeenCalledTimes(1);
+    expect(fail).toHaveBeenCalledTimes(1);
+    expect(end).toHaveBeenCalledTimes(1);
+  });
 
-  it.skip('should bail out on first failure', async () => {
-    jest.useRealTimers()
+  it('should bail out on first failure', async () => {
+    jest.useRealTimers();
 
     const fn = () => {
       return new Promise((resolve) => {
         setTimeout(() => {
-          resolve('Shouldn\'t resolve')
-        }, 1500)
-      })
-    }
+          resolve('Shouldn\'t resolve');
+        }, 1500);
+      });
+    };
 
     const errorFn = () => {
-      throw Error()
-    }
+      throw Error();
+    };
 
     // Non-sequential
-    const parent = new Suite('parent', defaultOpts, null)
-    const firstFail = new Test('firstFail', errorFn, defaultOpts, null)
-    const firstPass = new Test('firstPass', fn, defaultOpts, null)
-    const secondPass = new Test('secondPass', fn, defaultOpts, null)
+    const parent = new Suite('parent', defaultOpts, null);
+    const firstFail = new Test('firstFail', errorFn, defaultOpts, null);
+    const firstPass = new Test('firstPass', fn, defaultOpts, null);
+    const secondPass = new Test('secondPass', fn, defaultOpts, null);
 
-    parent.addChildren(firstFail, firstPass, secondPass)
+    parent.addChildren(firstFail, firstPass, secondPass);
 
-    const parentFail = jest.fn()
-    parent.on('fail', parentFail)
-    const parentPass = jest.fn()
-    parent.on('pass', parentPass)
-    const testFail = jest.fn()
-    firstFail.on('fail', testFail)
-    const testPass = jest.fn()
-    firstPass.on('pass', testPass)
+    const parentFail = jest.fn();
+    parent.on('fail', parentFail);
+    const parentPass = jest.fn();
+    parent.on('pass', parentPass);
+    const testFail = jest.fn();
+    firstFail.on('fail', testFail);
+    const testPass = jest.fn();
+    firstPass.on('pass', testPass);
 
-    expect((await parent.run({ bail: true, sequential: false })).status).toBe(Status.Failed)
-    expect(parent.getStats().done).toBe(1)
-    expect(testPass).toHaveBeenCalledTimes(0)
-    expect(testFail).toHaveBeenCalledTimes(1)
-    expect(parentPass).toHaveBeenCalledTimes(0)
-    expect(parentFail).toHaveBeenCalledTimes(1)
+    expect((await parent.run({ bail: true, sequential: false })).status).toBe(Status.Failed);
+    expect(parent.getStats().done).toBe(1);
+    expect(testPass).toHaveBeenCalledTimes(0);
+    expect(testFail).toHaveBeenCalledTimes(1);
+    expect(parentPass).toHaveBeenCalledTimes(0);
+    expect(parentFail).toHaveBeenCalledTimes(1);
 
     // Sequential
-    const sequentialParent = new Suite('sequentialParent', defaultOpts, null)
-    const secondFail = new Test('secondFail', errorFn, defaultOpts, null)
-    const thirdPass = new Test('thirdPass', fn, defaultOpts, null)
-    const fourthPass = new Test('fourthPass', fn, defaultOpts, null)
+    const sequentialParent = new Suite('sequentialParent', defaultOpts, null);
+    const secondFail = new Test('secondFail', errorFn, defaultOpts, null);
+    const thirdPass = new Test('thirdPass', fn, defaultOpts, null);
+    const fourthPass = new Test('fourthPass', fn, defaultOpts, null);
 
-    const sequentialParentPass = jest.fn()
-    sequentialParent.on('pass', sequentialParentPass)
-    const sequentialParentFail = jest.fn()
-    sequentialParent.on('fail', sequentialParentFail)
-    const sequentialTestPass = jest.fn()
-    thirdPass.on('pass', sequentialTestPass)
-    const sequentialTestFail = jest.fn()
-    secondFail.on('fail', sequentialTestFail)
+    const sequentialParentPass = jest.fn();
+    sequentialParent.on('pass', sequentialParentPass);
+    const sequentialParentFail = jest.fn();
+    sequentialParent.on('fail', sequentialParentFail);
+    const sequentialTestPass = jest.fn();
+    thirdPass.on('pass', sequentialTestPass);
+    const sequentialTestFail = jest.fn();
+    secondFail.on('fail', sequentialTestFail);
 
-    sequentialParent.addChildren(thirdPass, secondFail, fourthPass)
+    sequentialParent.addChildren(thirdPass, secondFail, fourthPass);
 
-    expect((await sequentialParent.run({ bail: true, sequential: true })).status).toBe(Status.Failed)
-    expect(sequentialParent.getStats().done).toBe(2)
-    expect(sequentialTestPass).toHaveBeenCalledTimes(1)
-    expect(sequentialTestFail).toHaveBeenCalledTimes(1)
-    expect(sequentialParentPass).toHaveBeenCalledTimes(0)
-    expect(sequentialParentFail).toHaveBeenCalledTimes(1)
-  })
+    expect((await sequentialParent.run({ bail: true, sequential: true })).status).toBe(Status.Failed);
+    expect(sequentialParent.getStats().done).toBe(2);
+    expect(sequentialTestPass).toHaveBeenCalledTimes(1);
+    expect(sequentialTestFail).toHaveBeenCalledTimes(1);
+    expect(sequentialParentPass).toHaveBeenCalledTimes(0);
+    expect(sequentialParentFail).toHaveBeenCalledTimes(1);
+  });
 
   it('should skip', async () => {
-    const parent = new Suite('parent', { skip: true }, null)
+    const parent = new Suite('parent', { skip: true }, null);
 
-    const start = jest.fn()
-    parent.on('start', start)
-    const skip = jest.fn()
-    parent.on('skip', skip)
-    const end = jest.fn()
-    parent.on('end', end)
+    const start = jest.fn();
+    parent.on('start', start);
+    const skip = jest.fn();
+    parent.on('skip', skip);
+    const end = jest.fn();
+    parent.on('end', end);
 
-    const promise = parent.run()
+    const promise = parent.run();
 
-    expect(skip).toHaveBeenCalledTimes(1)
-    expect(end).toHaveBeenCalledTimes(1)
-    expect((await promise).status).toBe(Status.Skipped)
-  })
+    expect(skip).toHaveBeenCalledTimes(1);
+    expect(end).toHaveBeenCalledTimes(1);
+    expect((await promise).status).toBe(Status.Skipped);
+  });
 
   it('should invoke hooks', async () => {
-    const error = console.error
-    console.error = jest.fn()
+    const error = console.error;
+    console.error = jest.fn();
 
-    const parent = new Suite('parent', {}, null)
+    const parent = new Suite('parent', {}, null);
     parent.addChildren(
       new Test('passing', jest.fn(), {}, parent),
-      new Test('failing', () => { throw new Error('Fail') }, {}, parent),
-    )
-    const calls: string[] = []
+      new Test('failing', () => { throw new Error('Fail'); }, {}, parent),
+    );
+    const calls: string[] = [];
 
     parent.hooks.beforeAll.push(
       () => calls.push('beforeAll1'),
       () => calls.push('beforeAll2'),
-    )
+    );
     parent.hooks.beforeEach.push(
       async () => await calls.push('beforeEach1'),
       () => calls.push('beforeEach2'),
-      () => { throw new Error('beforeEach hook error') },
-    )
+      () => { throw new Error('beforeEach hook error'); },
+    );
     parent.hooks.afterEach.push(
       () => calls.push('afterEach1'),
       () => calls.push('afterEach2'),
-    )
+    );
     parent.hooks.afterAll.push(
       () => calls.push('afterAll1'),
       async () => await calls.push('afterAll2'),
-      async () => { throw new Error('afterAll hook error') },
-    )
+      async () => { throw new Error('afterAll hook error'); },
+    );
 
     try {
-      await parent.run()
+      await parent.run();
     } catch {
       // noop
     }
-    expect(calls).toMatchSnapshot()
+    expect(calls).toMatchSnapshot();
 
-    expect(console.error).toHaveBeenCalledTimes(3)
-    expect(console.error).toHaveBeenCalledWith('Error in beforeEach hook: Error: beforeEach hook error')
-    expect(console.error).toHaveBeenCalledWith('Error in afterAll hook: Error: afterAll hook error')
+    expect(console.error).toHaveBeenCalledTimes(3);
+    expect(console.error).toHaveBeenCalledWith('Error in beforeEach hook: Error: beforeEach hook error');
+    expect(console.error).toHaveBeenCalledWith('Error in afterAll hook: Error: afterAll hook error');
 
-    console.error = error
-  })
-})
+    console.error = error;
+  });
+});
