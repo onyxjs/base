77e357c6b5183dae9f8f43a6c41a4e28efa12ba7
Elijah Kotyluk <elijah@elijahkotyluk.com>
2022-01-10 11:08:21 -0800

    chore(configs): update ts and jest.

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,6 +1,7 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  testEnvironment: 'node',
   roots: ['test/'],
-  testEnvironment: 'jsdom',
-  testMatch: ['**/test/**/*.[jt]s?(x)', '**/?(*.)+(spec).[jt]s?(x)'],
+  // testMatch: ['**/test/**/*.[jt]s?(x)', '**/?(*.)+(spec).[jt]s?(x)'],
 };
